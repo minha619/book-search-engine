3d48b1faefdcfbfb9849764a87294f901e269dcb
minha619 <minha619@gmail.com>
2022-01-24 00:45:47 -0500

    schemas/resolvers,typeDefs and index added

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,37 @@
 const { gql } = require('apollo-server-express');
 
+const typeDefs = gql`
+    type Book {
+        authors: [String]
+        description: String
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+    }
+
+    type Query {
+        me: User
+      }
+
+    type Auth {
+        token: ID!
+        user: User
+      }
+
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(book: bookInput): User
+        removeBook(bookId: String!): User
+      }
+`
 
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -11,15 +11,29 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
+
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.use(routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
